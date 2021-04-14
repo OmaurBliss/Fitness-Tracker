7be53df8b08af8c5165218f66e40262732fc2f67
Omaur B <omaurbliss@gmail.com>
2021-04-14 15:10:51 -0500

    added get workout api

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,24 @@
 
 const db = require("../models");
 const router = require("express").Router();
+
+//get workouts
+router.get("/api/workouts", (req, res) => {
+
+    db.Workout.find({}).then(dbWorkout => {
+        // console.log("ALL WORKOUTS");
+        // console.log(dbWorkout);
+        dbWorkout.forEach(workout => {
+            var total = 0;
+            workout.exercises.forEach(e => {
+                total += e.duration;
+            });
+            workout.totalDuration = total;
+
+        });
+
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.json(err);
+    });
+});
