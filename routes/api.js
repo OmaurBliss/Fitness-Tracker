a033c82c6bcf1c01db7a350eb639bf7a073ef27d
Omaur B <omaurbliss@gmail.com>
2021-04-14 15:12:58 -0500

    add exercise api route

--- routes/api.js
+++ routes/api.js
@@ -22,3 +22,20 @@ router.get("/api/workouts", (req, res) => {
         res.json(err);
     });
 });
+
+// add exercise
+router.put("/api/workouts/:id", (req, res) => {
+
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+});
